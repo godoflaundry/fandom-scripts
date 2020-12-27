b6459f389989e79125e0b4fe4260f417074cf91d
godoflaundry <56318296+godoflaundry@users.noreply.github.com>
2020-12-26 20:27:50 -0600

    add relatedwork script

---
+++ tapermonkey/relatedwork.pub.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Related Work Button
+// @version      0.1
+// @description  Adds a Related Works button to the ao3 user page
+// @author       godoflaundrybaskets
+// @include      http*://archiveofourown.org/*users*
+// @require      http://code.jquery.com/jquery-3.5.1.min.js
+// ==/UserScript==
+const user_regex = /https:\/\/archiveofourown\.org\/users\/([^/]+)/;
+
+$( document ).ready(function() {
+    let m = window.location.href.match(user_regex);
+    if (m === null) {
+        return;
+    }
+    console.log("On a profile, fetching bp info...")
+
+    $("#dashboard ul").first().append(
+        `<li><a href="${m[0]}/related_works?target=blank">Related Works</a></li>`
+    )
+
+});
